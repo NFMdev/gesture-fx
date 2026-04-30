40c7570e6a59dc33ec662d23af8f0c9ac53e1f51
Nico Fiorito <88680241+NFMdev@users.noreply.github.com>
2026-04-30 21:03:21 +0200

    + Start vite project + Create camera control functions + Create camera control buttons and preview

---
+++ src/camera.ts
@@ -0,0 +1,31 @@
+export async function captureCamera(mediaRecorder: MediaRecorder): Promise<MediaRecorder> {
+    try {
+        const stream: MediaStream = await navigator.mediaDevices.getUserMedia({
+                video: true
+        });
+
+        if (!mediaRecorder) {
+            mediaRecorder = new MediaRecorder(stream);
+        }
+
+        const videoElement = document.getElementById('preview') as HTMLVideoElement;
+        videoElement.srcObject = stream;
+        const chunks: Blob[]  = [];
+
+        mediaRecorder.ondataavailable = (e: BlobEvent) => {
+            if (e.data.size > 0) chunks.push(e.data);
+        }
+
+        mediaRecorder.start();
+        return await mediaRecorder;
+    } catch (error) {
+        console.log('Camera error:', error);
+        throw new Error("Failed to get media recorder");
+    }
+}
+
+export async function cameraStop(mediaRecorder: MediaRecorder) {
+    mediaRecorder.stop();
+    const videoElement = document.getElementById('preview') as HTMLVideoElement;
+    videoElement.srcObject = null;
+}
---
+++ src/main.ts
@@ -0,0 +1,78 @@
+import './style.css'
+import typescriptLogo from './assets/typescript.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import { cameraStop, captureCamera } from './camera'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+<section id="center">
+  <div class="hero">
+    <img src="${heroImg}" class="base" width="170" height="179">
+    <img src="${typescriptLogo}" class="framework" alt="TypeScript logo"/>
+    <img src="${viteLogo}" class="vite" alt="Vite logo" />
+  </div>
+  <div>
+    <button type="button" id="buttonStart">Start</button>
+    <button type="button" id="buttonStop" disabled>Stop</button>
+  </div>
+  <div>
+  <div>
+    <video width="640" height="360" autoplay muted playsinline id="preview"/>
+  </div>
+</section>
+
+<div class="ticks"></div>
+
+<section id="next-steps">
+  <div id="docs">
+    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
+    <h2>Documentation</h2>
+    <p>Your questions, answered</p>
+    <ul>
+      <li>
+        <a href="https://vite.dev/" target="_blank">
+          <img class="logo" src="${viteLogo}" alt="" />
+          Explore Vite
+        </a>
+      </li>
+      <li>
+        <a href="https://www.typescriptlang.org" target="_blank">
+          <img class="button-icon" src="${typescriptLogo}" alt="">
+          Learn more
+        </a>
+      </li>
+    </ul>
+  </div>
+  <div id="social">
+    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
+    <h2>Connect with us</h2>
+    <p>Join the Vite community</p>
+    <ul>
+      <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
+      <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
+      <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
+      <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
+    </ul>
+  </div>
+</section>
+
+<div class="ticks"></div>
+<section id="spacer"></section>
+`
+
+
+const buttonStart = document.getElementById('buttonStart');
+const buttonStop = document.getElementById('buttonStop');
+let mediaRecorder: MediaRecorder;
+
+buttonStart?.addEventListener('click', async () => {
+  mediaRecorder = await captureCamera(mediaRecorder);
+  buttonStart.setAttribute('disabled', '');
+  buttonStop?.removeAttribute('disabled');
+});
+
+buttonStop?.addEventListener('click', () => {
+  cameraStop(mediaRecorder);
+  buttonStop.setAttribute('disabled', '');
+  buttonStart?.removeAttribute('disabled');
+});
